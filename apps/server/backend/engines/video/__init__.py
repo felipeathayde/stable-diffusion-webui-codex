7fb7a19e92d72406c0eb9a06b0cb4b0f17127695
Lucas Freire Sangoi <lucas.sangoi@avmb.com.br>
2025-10-24 20:29:46 -0300

    chore(engines): remove legacy WAN video engines (wan_i2v_14b, wan_t2v_14b)

--- apps/server/backend/engines/video/__init__.py
+++
@@ -1,6 +0,0 @@
-from .wan import WanI2V14BEngine, WanT2V14BEngine
-
-__all__ = [
-    "WanI2V14BEngine",
-    "WanT2V14BEngine",
-]
--- apps/server/backend/engines/video/wan/__init__.py
+++
@@ -1,4 +0,0 @@
-from .i2v14b_engine import WanI2V14BEngine
-from .t2v14b_engine import WanT2V14BEngine
-
-__all__ = ["WanI2V14BEngine", "WanT2V14BEngine"]
